2caf009339a16e1168b2fd6de3c3e8bd30230fae
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-15 08:35:37 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,15 +1,28 @@
-import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext"; // adjust path
+// import { Navigate, useLocation } from "react-router-dom";
+// import { useAuth } from "../contexts/AuthContext"; // adjust path
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const location = useLocation();
+// const ProtectedRoute = ({ component: Component, ...rest }) => {
+//   const location = useLocation();
+//   const { isAuthenticated } = useAuth();
+//   console.log("status:",isAuthenticated);
+
+//   return isAuthenticated ? (
+//     <Component {...rest} />
+//   ) : (
+//     <Navigate to="/login" state={{ from: location }} replace />
+//   );
+// };
+// export default ProtectedRoute;
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
+  const location = useLocation();
   console.log("status:",isAuthenticated);
 
-  return isAuthenticated ? (
-    <Component {...rest} />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+  return isAuthenticated ? children : <Navigate to="/login" state={{ from: location }} replace />;
 };
+
 export default ProtectedRoute;
+
