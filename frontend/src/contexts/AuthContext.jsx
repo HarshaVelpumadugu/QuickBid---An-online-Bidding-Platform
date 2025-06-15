328de698df6f9415836a9f3681a319401e23ded4
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-15 07:54:30 +0530

    Update AuthContext.jsx

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,9 +1,4 @@
 // AuthContext.js
-import { createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
-
-const AuthContext = createContext();
-
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
@@ -21,11 +16,17 @@ export const AuthProvider = ({ children }) => {
     check();
   }, []);
 
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
-
-export const useAuth = () => useContext(AuthContext);
