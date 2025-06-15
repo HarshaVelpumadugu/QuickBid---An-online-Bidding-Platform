bff1e0aa9ba5038ef7bb9ca9695a9544e9816bc1
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-15 07:28:54 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,22 +1,40 @@
 import { Navigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-	const location = useLocation();
-	const token = document.cookie
-		.split("; ")
-		.find((row) => row.startsWith("jwt="))
-		?.split("=")[1];
-	console.log("token: ",token);
-	return token ? (
-		<Component {...rest} />
-	) : (
-		<Navigate to="/login" state={{ from: location }} replace />
-	);
+  const location = useLocation();
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    const check = async () => {
+      try {
+        const res = await axios.get("https://quickbid-backend.onrender.com/api/users/check-auth", {
+          withCredentials: true,
+        });
+        if (res.status === 200) {
+          setIsAuthenticated(true);
+        }
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+    check();
+  }, []);
+
+  if (isAuthenticated === null) return <div>Loading...</div>;
+
+  return isAuthenticated ? (
+    <Component {...rest} />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 };
 
 ProtectedRoute.propTypes = {
-	component: PropTypes.elementType.isRequired,
+  component: PropTypes.elementType.isRequired,
 };
 
 export default ProtectedRoute;
+
